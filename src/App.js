349d11609888f0e8c7018504a88b4249671eebd1
Chris Guevara <chrismguevara@gmail.com>
2016-09-28 21:42:02 -0700

    Pass event handlers and state to lineitemlist component

--- src/App.js
+++ src/App.js
@@ -15,6 +15,91 @@ import {
 import LineItemList from './LineItemList.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      invoiceNumber: '',
+      fromName: '',
+      toName: '',
+      date: '',
+      dueDate: '',
+      balanceDue: 0,
+      lineItems: [{
+        description: '',
+        quantity: 0,
+        rate: 0
+      }]
+    };
+
+    this.onFieldValueChange = this.onFieldValueChange.bind(this);
+    this.onLineItemDescriptionChange = this.onLineItemDescriptionChange.bind(this);
+    this.onLineItemQuantityChange = this.onLineItemQuantityChange.bind(this);
+    this.onLineItemRateChange = this.onLineItemRateChange.bind(this);
+    this.onLineItemDeleteClick = this.onLineItemDeleteClick.bind(this);
+    this.onLineItemAddClick = this.onLineItemAddClick.bind(this);
+  }
+
+  onInvoiceNumberChange(event) {
+    let newVal = event.target.value;
+    this.setState({
+      invoiceNumber: newVal
+    });
+  }
+
+  onFieldValueChange(propertyName, event) {
+    let newVal = event.target.value;
+    let stateUpdate = {};
+    stateUpdate[propertyName] = newVal;
+    this.setState(stateUpdate);
+  }
+
+  onLineItemDescriptionChange(params) {
+    let lineItems = this.state.lineItems;
+    let lineItem = lineItems[params.index]
+    lineItem.description = params.newDescription;
+    this.setState({
+      lineItems: lineItems
+    });
+  }
+
+  onLineItemQuantityChange(params) {
+    let lineItems = this.state.lineItems;
+    let lineItem = lineItems[params.index];
+    lineItem.quantity = params.newQuantity;
+    this.setState({
+      lineItems: lineItems
+    });
+  }
+
+  onLineItemRateChange(params) {
+    let lineItems = this.state.lineItems;
+    let lineItem = lineItems[params.index];
+    lineItem.rate = params.newRate;
+    this.setState({
+      lineItems: lineItems
+    });
+  }
+
+  onLineItemDeleteClick(params) {
+    let lineItems = this.state.lineItems;
+    lineItems.splice(params.index, 1);
+    this.setState({
+      lineItems: lineItems
+    });
+  }
+
+  onLineItemAddClick() {
+    let lineItems = this.state.lineItems;
+    lineItems.push({
+      description: '',
+      quantity: 0,
+      rate: 0
+    });
+    this.setState({
+      lineItems: lineItems
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -27,7 +112,7 @@ class App extends Component {
                 Invoice #
               </Col>
               <Col sm={10}>
-                <FormControl type="text" placeholder="4" />
+                <FormControl type="text" placeholder="4" value={this.state.invoiceNumber} onChange={this.onFieldValueChange.bind(this, 'invoiceNumber')}/>
               </Col>
             </FormGroup>
             <FormGroup controlId="fromName">
@@ -35,7 +120,7 @@ class App extends Component {
                 From
               </Col>
               <Col sm={10}>
-                <FormControl type="text" placeholder="4" />
+                <FormControl type="text" placeholder="4" value={this.state.fromName} onChange={this.onFieldValueChange.bind(this, 'fromName')}/>
               </Col>
             </FormGroup>
             <FormGroup controlId="toName">
@@ -43,7 +128,7 @@ class App extends Component {
                 Bill To
               </Col>
               <Col sm={10}>
-                <FormControl type="text" placeholder="4" />
+                <FormControl type="text" placeholder="4" value={this.state.toName} onChange={this.onFieldValueChange.bind(this, 'toName')}/>
               </Col>
             </FormGroup>
             <FormGroup controlId="date">
@@ -51,7 +136,7 @@ class App extends Component {
                 Date
               </Col>
               <Col sm={10}>
-                <FormControl type="date" />
+                <FormControl type="date" value={this.state.date} onChange={this.onFieldValueChange.bind(this, 'date')}/>
               </Col>
             </FormGroup>
             <FormGroup controlId="dueDate">
@@ -59,7 +144,7 @@ class App extends Component {
                 Due Date
               </Col>
               <Col sm={10}>
-                <FormControl type="date" />
+                <FormControl type="date" value={this.state.dueDate} onChange={this.onFieldValueChange.bind(this, 'dueDate')}/>
               </Col>
             </FormGroup>
             <FormGroup controlId="balanceDue">
@@ -67,10 +152,18 @@ class App extends Component {
                 Balance Due
               </Col>
               <Col sm={10}>
-                <FormControl type="number" min="0.01" step="0.01" />
+                <FormControl type="number" min="0.01" step="0.01" value={this.state.balanceDue} onChange={this.onFieldValueChange.bind(this, 'balanceDue')}/>
               </Col>
             </FormGroup>
-            <LineItemList></LineItemList>
+            <LineItemList
+              lineItems={this.state.lineItems}
+              onLineItemDescriptionChange={this.onLineItemDescriptionChange}
+              onLineItemQuantityChange={this.onLineItemQuantityChange}
+              onLineItemRateChange={this.onLineItemRateChange}
+              onLineItemDeleteClick={this.onLineItemDeleteClick}
+              onLineItemAddClick={this.onLineItemAddClick}
+              >
+            </LineItemList>
           </Form>
         </div>
       </div>
---
+++ src/LineItemList.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+import {
+  Button,
+  Grid,
+  Row,
+  Col,
+  FormControl
+} from 'react-bootstrap';
+
+class LineItemList extends Component {
+  onLineItemDescriptionChange(index, event) {
+    this.props.onLineItemDescriptionChange({
+      index: index,
+      newDescription: event.target.value
+    });
+  }
+
+  onLineItemQuantityChange(index, event) {
+    this.props.onLineItemQuantityChange({
+      index: index,
+      newQuantity: event.target.value
+    });
+  }
+
+  onLineItemRateChange(index, event) {
+    this.props.onLineItemRateChange({
+      index: index,
+      newRate: event.target.value
+    });
+  }
+
+  onLineItemDeleteClick(index) {
+    this.props.onLineItemDeleteClick({
+      index: index
+    });
+  }
+
+	render() {
+		let lineItems = this.props.lineItems;
+		let lineItemRows = lineItems.map((lineItem, index) => {
+			return (
+			  <Row key={index}>
+              <Col sm={8}>
+                <FormControl type="text" value={lineItem.description} onChange={this.onLineItemDescriptionChange.bind(this, index)}/>
+              </Col>
+              <Col sm={1}>
+                <FormControl type="number" value={lineItem.quantity} onChange={this.onLineItemQuantityChange.bind(this, index)}/>
+              </Col>
+              <Col sm={1}>
+                <FormControl type="number" value={lineItem.rate} onChange={this.onLineItemRateChange.bind(this, index)}/>
+              </Col>
+              <Col sm={1}>
+                {lineItem.quantity * lineItem.rate}
+              </Col>
+              <Col sm={1}>
+                <Button onClick={this.onLineItemDeleteClick.bind(this, index)}>X</Button>
+              </Col>
+            </Row>
+		)});
+		let lineItemsTotal = lineItems.reduce((sum, lineItem) => {
+			return sum + lineItem.quantity * lineItem.rate;
+		}, 0);
+		return (
+            <Grid>
+              <Row>
+                <Col sm={8}>Item</Col>
+                <Col sm={1}>Quantity</Col>
+                <Col sm={1}>Rate</Col>
+                <Col sm={1}>Amount</Col>
+                <Col sm={1}></Col>
+              </Row>
+              {lineItemRows}
+              <Row>
+              	<Col sm={8}>
+                  <Button onClick={this.props.onLineItemAddClick}>+ Add Line Item</Button>
+                </Col>
+                <Col sm={1}></Col>
+                <Col sm={1}>Subtotal</Col>
+                <Col sm={1}>{lineItemsTotal}</Col>
+                <Col sm={1}></Col>
+              </Row>
+            </Grid>
+          );
+	}
+}
+
+export default LineItemList;
