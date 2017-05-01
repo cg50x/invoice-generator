6ebec48cf5fdb61518a369a52320c1a71bfb6bee
Karl Horky <karl.horky@gmail.com>
2017-05-01 18:56:37 +0200

    Format code with Prettier

--- src/LineItemList.js
+++ src/LineItemList.js
@@ -1,39 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
-import {
-  Button,
-  Row,
-  Col,
-  FormControl
-} from 'react-bootstrap';
+import {Button, Row, Col, FormControl} from 'react-bootstrap';
 
 class LineItemList extends Component {
   onLineItemDescriptionChange(index, event) {
     this.props.onLineItemDescriptionChange({
       index: index,
-      newDescription: event.target.value
+      newDescription: event.target.value,
     });
   }
 
   onLineItemQuantityChange(index, event) {
     this.props.onLineItemQuantityChange({
       index: index,
-      newQuantity: event.target.value
+      newQuantity: event.target.value,
     });
   }
 
   onLineItemRateChange(index, event) {
     this.props.onLineItemRateChange({
       index: index,
-      newRate: event.target.value
+      newRate: event.target.value,
     });
   }
 
   onLineItemDeleteClick(index) {
     this.props.onLineItemDeleteClick({
-      index: index
+      index: index,
     });
   }
 
@@ -45,61 +40,71 @@ class LineItemList extends Component {
 
   renderLineItemRow(lineItem, index) {
     return (
-        <Row key={index}>
-          <Col sm={8}>
-            <FormControl
-              type="text"
-              value={lineItem.description}
-              onChange={this.onLineItemDescriptionChange.bind(this, index)}/>
-          </Col>
-          <Col sm={1} style={{ paddingLeft: '7px', paddingRight: '7px' }}>
-            <FormControl
-              type="number"
-              value={lineItem.quantity}
-              onChange={this.onLineItemQuantityChange.bind(this, index)}/>
-          </Col>
-          <Col sm={1} style={{ paddingLeft: '7px', paddingRight: '7px' }}>
-            <FormControl
-              type="number"
-              value={lineItem.rate}
-              onChange={this.onLineItemRateChange.bind(this, index)}/>
-          </Col>
-          <Col sm={1}>
-            {lineItem.quantity * lineItem.rate}
-          </Col>
-          <Col sm={1}>
-            <Button bsStyle="danger" onClick={this.onLineItemDeleteClick.bind(this, index)}>X</Button>
-          </Col>
-        </Row>
+      <Row key={index}>
+        <Col sm={8}>
+          <FormControl
+            type="text"
+            value={lineItem.description}
+            onChange={this.onLineItemDescriptionChange.bind(this, index)}
+          />
+        </Col>
+        <Col sm={1} style={{paddingLeft: '7px', paddingRight: '7px'}}>
+          <FormControl
+            type="number"
+            value={lineItem.quantity}
+            onChange={this.onLineItemQuantityChange.bind(this, index)}
+          />
+        </Col>
+        <Col sm={1} style={{paddingLeft: '7px', paddingRight: '7px'}}>
+          <FormControl
+            type="number"
+            value={lineItem.rate}
+            onChange={this.onLineItemRateChange.bind(this, index)}
+          />
+        </Col>
+        <Col sm={1} />
+          {lineItem.quantity * lineItem.rate}
+        </Col>
+        <Col sm={1}>
+          <Button
+            bsStyle="danger"
+            onClick={this.onLineItemDeleteClick.bind(this, index)}
+          >
+            X
+          </Button>
+        </Col>
+      </Row>
     );
   }
 
-	render() {
-		let lineItems = this.props.lineItems;
-		let lineItemRows = lineItems.map(this.renderLineItemRow.bind(this));
-		let lineItemsTotal = this.getLineItemsTotal(lineItems);
-		return (
+  render() {
+    let lineItems = this.props.lineItems;
+    let lineItemRows = lineItems.map(this.renderLineItemRow.bind(this));
+    let lineItemsTotal = this.getLineItemsTotal(lineItems);
+    return (
       <div>
         <Row>
           <Col sm={8}>Item</Col>
           <Col sm={1}>Quantity</Col>
           <Col sm={1}>Rate</Col>
           <Col sm={1}>Amount</Col>
-          <Col sm={1}></Col>
+          <Col sm={1} />
         </Row>
         {lineItemRows}
         <Row>
-        	<Col sm={8}>
-            <Button bsStyle="success" onClick={this.props.onLineItemAddClick}>+ Add Line Item</Button>
+          <Col sm={8}>
+            <Button bsStyle="success" onClick={this.props.onLineItemAddClick}>
+              + Add Line Item
+            </Button>
           </Col>
-          <Col sm={1}></Col>
+          <Col sm={1} />
           <Col sm={1}>Total</Col>
           <Col sm={1}>{lineItemsTotal}</Col>
           <Col sm={1}></Col>
         </Row>
       </div>
-          );
-	}
+    );
+  }
 }
 
 LineItemList.propTypes = {
@@ -107,11 +112,11 @@ LineItemList.propTypes = {
   onLineItemDescriptionChange: React.PropTypes.func,
   onLineItemQuantityChange: React.PropTypes.func,
   onLineItemRateChange: React.PropTypes.func,
-  onLineItemDeleteClick: React.PropTypes.func
-}
+  onLineItemDeleteClick: React.PropTypes.func,
+};
 
 LineItemList.defaultProps = {
-  lineItems: []
+  lineItems: [],
 };
 
 export default LineItemList;
